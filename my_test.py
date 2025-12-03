a20cbaa4830234fedb157037fec4bb4c214fb80c
jeong insu <jenginsu@gmail.com>
2025-12-03 14:58:13 +0900

    관리자 비밀번호 알기 함수 추가

--- my_test.py
+++ my_test.py
@@ -1,5 +1,6 @@
-user_id = "jenginsu"
-user_pwd = "1234"
+import sqlite3
+conn = sqlite3.connect('test.db')
+cursor = conn.cursor()
 
 def login_check(user_id, user_pwd):
     if user_id == "jenginsu" and user_pwd == "1234":
@@ -7,5 +8,14 @@ def login_check(user_id, user_pwd):
     else:
         print(f"user_id={user_id}, user_pwd={user_pwd} login failed")
 
+def get_admin_pwd():
+    get_admin_pwd_query = "SELECT password FROM users WHERE user_id='admin'"
+    cursor.execute(get_admin_pwd_query)
+    admin_pwd = cursor.fetchone()
+    print(f"admin_pwd={admin_pwd}")
+
 if __name__ == "__main__":
+    user_id = "jenginsu"
+    user_pwd = "1234"
     login_check(user_id, user_pwd)
+    get_admin_pwd()
